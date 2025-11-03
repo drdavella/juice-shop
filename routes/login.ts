687371a150c9dda4626f698970ec4fd484aba3ae
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2025-11-03 21:24:35 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- routes/login.ts
+++ routes/login.ts
@@ -35,7 +35,7 @@ module.exports = function login () {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
     models.sequelize.query('SELECT * FROM Users WHERE email = :email AND password = :password AND deletedAt IS NULL', {
       replacements: {
-        email: req.body.email || '', 
+        email: req.body.email || '',
         password: security.hash(req.body.password || '')
       },
       model: UserModel,
